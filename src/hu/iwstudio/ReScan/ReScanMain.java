0b54980a74aee775ae49be8d55d084b6cbc567e1
Eisenberger Tamás <tamas@eisenberger.hu>
2013-05-17 20:00:42 +0200

    Migrated to android studio

--- src/hu/iwstudio/ReScan/ReScanMain.java
+++ src/hu/iwstudio/ReScan/ReScanMain.java
@@ -10,51 +10,50 @@ import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 
 public class ReScanMain extends Activity {
-	
-	BroadcastReceiver mFinishReceiver;
-	ProgressDialog mDialog;
-	
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		Resources res = getResources();
-		
-		mDialog = ProgressDialog.show(this, res.getString(R.string.app_name), 
-				res.getString(R.string.dialog_text), true);
-		
-		IntentFilter finishFilter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_FINISHED);
-		finishFilter.addDataScheme("file");
-		
-		mFinishReceiver = new BroadcastReceiver() {
-			public void onReceive(Context context, Intent intent) {
-				finish();
-	        }
-	    };
 
-	    registerReceiver(mFinishReceiver, finishFilter);
-		
-		
-		sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
-	}
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-		mDialog.show();
-	}
+    BroadcastReceiver mFinishReceiver;
+    ProgressDialog mDialog;
 
-	@Override
-	protected void onPause() {
-		mDialog.dismiss();
-		super.onPause();
-	}
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Resources res = getResources();
 
-	@Override
-	protected void onDestroy() {
-		unregisterReceiver(mFinishReceiver);
-		super.onDestroy();
-	}
-	
+        mDialog = ProgressDialog.show(this, res.getString(R.string.app_name),
+                res.getString(R.string.dialog_text), true);
+
+        IntentFilter finishFilter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_FINISHED);
+        finishFilter.addDataScheme("file");
+
+        mFinishReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                finish();
+            }
+        };
+
+        registerReceiver(mFinishReceiver, finishFilter);
+
+
+        sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://" + Environment.getExternalStorageDirectory())));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mDialog.show();
+    }
+
+    @Override
+    protected void onPause() {
+        mDialog.dismiss();
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mFinishReceiver);
+        super.onDestroy();
+    }
+
 }
